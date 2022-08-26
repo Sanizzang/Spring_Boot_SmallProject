d16d10b67413b62ecc5bd90c687957dcab164f52
Sanizzang <tksl4732@naver.com>
2022-08-26 15:25:30 +0900

    complete ch_22

--- firstproject/firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,22 +1,21 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // DB가 해당 객체를 인식 가능!
 @AllArgsConstructor
 @NoArgsConstructor // 디폴트 생성자를 추가!
 @ToString
+@Getter
 public class Article {
 
     @Id // 대표값을 지정! like a 주민등록번호
-    @GeneratedValue // 1, 2, 3, .... 자동 생성 어노테이션!
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id를 자동 생성 어노테이션!
     private Long id;
 
     @Column
@@ -24,4 +23,11 @@ public class Article {
 
     @Column
     private String content;
+
+    public void patch(Article article) {
+        if (article.title != null)
+            this.title = article.title;
+        if (article.content != null)
+            this.content = article.content;
+    }
 }
